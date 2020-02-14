b72ce75d69c1701957235ee5e643f2ff39474762
刘伟 <liuw@hsmap.com>
2020-02-14 11:07:53 +0800

    add train.sh,predict.sh

---
+++ train.sh
@@ -0,0 +1,1 @@
+python albert_ner_clinical.py --task_name ner --do_train true --do_eval true --data_dir data/clinical_data --vocab_file ./albert_config/vocab.txt --bert_config_file ./albert_base_zh/albert_config_base.json --max_seq_length 128 --train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 --output_dir albert_base_ner_checkpoints
